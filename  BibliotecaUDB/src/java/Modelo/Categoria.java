111ddc0ea6e1c6bf7beece3dc7303dcbe3ecb299
lirio90 <lilian.recinos@gmail.com>
2020-06-13 18:06:41 -0600

    Categoria

---
+++  BibliotecaUDB/src/java/Modelo/Categoria.java
@@ -0,0 +1,50 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Bolaines
+ */
+public class Categoria {
+    
+    
+    private int  codCategoria;
+    private String nombre;
+
+    
+      public Categoria(int codCategoria, String nombre) {
+        this.codCategoria = codCategoria;
+        this.nombre = nombre;
+    }
+
+      
+    public Categoria() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    public int getCodCategoria() {
+        return codCategoria;
+    }
+
+    public void setCodCategoria(int codCategoria) {
+        this.codCategoria = codCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+  
+    
+    
+    
+    
+    
+    
+}// cierre
