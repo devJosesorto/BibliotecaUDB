ecd00685f395abf0266d38cc1d71097fa416817e
lirio90 <lilian.recinos@gmail.com>
2020-06-13 18:16:41 -0600

    CategoriaSql

---
+++  BibliotecaUDB/src/java/ModeoDAO/CategoriaSQL.java
@@ -0,0 +1,177 @@
+
+package ModeoDAO;
+
+import Conexion.Conexion;
+import Modelo.Categoria;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Bolaines
+ */
+public class CategoriaSQL extends Conexion {
+    
+ 
+     public List Mostrar() {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = getConexion();
+
+        ArrayList<Categoria> list = new ArrayList<>();
+
+        String sql = "SELECT* FROM categoria";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                Categoria categoria = new Categoria();
+
+                categoria.setCodCategoria(rs.getInt(1));
+                categoria.setNombre(rs.getString(2));
+                
+                list.add(categoria);
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e);
+
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return list;
+    }
+    
+        
+     public boolean Agregar(boolean pass, Categoria categoria) {
+
+        if (pass) {
+            PreparedStatement ps = null;
+            Connection con = getConexion();
+            String sql = "INSERT INTO categoria (codCategoria, Nombres) VALUES(?,?)";
+
+            try {
+                int z = 1;
+                ps = con.prepareStatement(sql);
+                ps.setInt(z++, generarCod());
+                ps.setString(z++, categoria.getNombre());
+                ps.setInt(z++, categoria.getCodCategoria());
+
+                ps.execute();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+                System.out.println("Error en Agregar de la clase AutorSQL");
+                return false;
+
+            } finally {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+        return true;
+    }
+    
+    
+     public void Actualizar(boolean pass, Categoria categoria) {
+        if (pass) {
+            PreparedStatement ps = null;
+            Connection con = getConexion();
+            String sql = "UPDATE categoria SET Nombre=?, codCategoria=? WHERE codCategoria=? ";
+
+            try {
+                ps = con.prepareStatement(sql);
+                int z = 1;
+                ps.setString(z++, categoria.getNombre());
+                ps.setInt(z++, categoria.getCodCategoria());
+                
+                ps.execute();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+                System.out.println("Error en Actualizar de la clase CategoriaSQL");
+
+            } finally {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+     
+    
+    public void Eliminar(boolean pass, String codLibro) {
+        if (pass) {
+            PreparedStatement ps = null;
+            Connection con = getConexion();
+            String sql = "DELETE FROM Categoria WHERE categoria=? ";
+
+            try {
+                ps = con.prepareStatement(sql);
+                ps.setString(1, sql);
+                ps.execute();
+
+            } catch (SQLException e) {
+                System.err.println(e);
+                System.out.println("Error en Eliminar de la clase CategoriaSQL");
+
+            } finally {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+    
+     
+         
+     ///Genarador de codigo
+     
+     public int generarCod() {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = getConexion();
+
+        String sql = "SELECT MAX(codCategoria) as cantidad FROM categoria";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return Integer.parseInt(rs.getString("cantidad")) + 1;
+            }
+            return 0;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return 0;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+     
+     
+     
+     
+    
+    
+}// cierre
